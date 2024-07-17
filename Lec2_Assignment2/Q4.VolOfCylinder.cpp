9bc36775f6f7cda22ee3ed7fed4685025e10451f
Sidharth Kumar <kumarsid556@gmail.com>
2024-07-17 21:33:57 +0530

    Add files via upload

---
+++ Lec2_Assignment2/Q4.VolOfCylinder.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r,h,vol;
+    cout<<"Enter Radius ";
+    cin>>r;
+    cout<<"Enter Height ";
+    cin>>h;
+    vol = 3.1415*r*r*h;
+    cout<<"Volume of Cylinder is "<<vol;
+}
---
+++ Lec2_Assignment2/Q5DifferenceOfASCII.cpp
@@ -0,0 +1,15 @@
+// WAP to find the difference between ASCII of two characters,take them as input
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a,b;
+    cout<<"Enter First Character ";
+    cin>>a;
+    cout<<"Enter Second Character ";
+    cin>>b;
+    int x = (int)a;
+    int y = (int)b;
+    cout<<x-y;
+}
+
