61c39b5e8e7732cc3822e6ccbe4b5ea83738c8ba
Sidharth Kumar <kumarsid556@gmail.com>
2024-07-17 16:00:53 +0530

    Add files via upload

---
+++ Lec1_Assignment/Q3.cpp
@@ -0,0 +1,3 @@
+// int - 2/4 byte;
+// bool - 1 byte;
+// float - 4 byte
---
+++ Lec1_Assignment/Q5.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius = 10;
+    float c = 2*3.1415*radius;
+    cout<<c;
+}
